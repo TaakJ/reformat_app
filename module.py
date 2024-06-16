40d898e8a157794429c0898974dca03ed698300e
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-06-16 23:04:56 +0700

    for testing

--- module.py
+++ module.py
@@ -13,6 +13,7 @@ import chardet
 from io import StringIO
 import re
 import xlrd
+import csv
 
 
 class convert_2_files:
@@ -20,7 +21,7 @@ class convert_2_files:
     async def check_source_files(self) -> None:
 
         logging.info("Check Source files..")
-        
+
         set_log = []
         for _dir in self.input_dir:
 
@@ -39,14 +40,15 @@ class convert_2_files:
 
         self._log_setter(set_log)
 
-
     async def retrieve_data_from_source_files(self) -> None:
 
         logging.info("Retrieve Data from Source files..")
 
         state = "failed"
         for i, record in enumerate(self.logging):
-            record.update({"function": "retrieve_data_from_source_files", "state": state})
+            record.update(
+                {"function": "retrieve_data_from_source_files", "state": state}
+            )
 
             _dir = record["input_dir"]
             types = Path(_dir).suffix
@@ -70,7 +72,6 @@ class convert_2_files:
 
             if "errors" in record:
                 raise CustomException(errors=self.logging)
-            
 
     def read_text_files(func):
         def wrapper(*args: tuple, **kwargs: dict) -> dict:
@@ -129,7 +130,6 @@ class convert_2_files:
 
         return wrapper
 
-
     @read_text_files
     def text_data_cleaning(self, i: int) -> any:
 
@@ -148,9 +148,7 @@ class convert_2_files:
             regex = re.compile(r"\w+.*")
             find_regex = regex.findall(lines)
             if find_regex != []:
-                yield {sheets: re.sub(r"\W\s+", "||", ""\
-                    .join(find_regex).strip()).split("||")}
-
+                yield {sheets: re.sub(r"\W\s+","||","".join(find_regex).strip()).split("||")}
 
     def read_excle_files(func):
         def wrapper(*args: tuple, **kwargs: dict) -> dict:
@@ -161,8 +159,11 @@ class convert_2_files:
             while True:
                 try:
                     for sheets, data in next(by_sheets).items():
-                        if not all(dup == data[0] for dup in data) and \
-                            not data.__contains__("Centralized User Management : User List."):
+                        if not all(
+                            dup == data[0] for dup in data
+                        ) and not data.__contains__(
+                            "Centralized User Management : User List."
+                        ):
                             if sheets not in _data:
                                 _data[sheets] = [data]
                             else:
@@ -174,7 +175,6 @@ class convert_2_files:
 
         return wrapper
 
-
     @read_excle_files
     def excel_data_cleaning(self, i: int) -> any:
 
@@ -182,27 +182,27 @@ class convert_2_files:
         self.logging[i].update({"function": "excel_data_cleaning"})
 
         workbook = xlrd.open_workbook(self.logging[i]["input_dir"])
-        sheet_list = [sheet for sheet in workbook.sheet_names() if sheet != "StyleSheet"]
+        sheet_list = [
+            sheet for sheet in workbook.sheet_names() if sheet != "StyleSheet"
+        ]
 
         for sheets in sheet_list:
             cells = workbook.sheet_by_name(sheets)
             for row in range(0, cells.nrows):
-                yield {sheets: [cells.cell(row, col)\
-                    .value for col in range(cells.ncols)]}
-
+                yield {sheets: [cells.cell(row, col).value for col in range(cells.ncols)]}
 
     async def write_data_to_tmp_file(self) -> None:
 
         logging.info("Write Data to Tmp files..")
 
-        template_name = join(Folder.TEMPLATE, "Application Data Requirements.xlsx")
-        tmp_name = join(Folder.TMP, f"TMP_{self.module}-{self.batch_date.strftime('%d%m%y')}.xlsx")
-
         state = "failed"
         for record in self.logging:
             try:
                 if record["module"] == "Target_file":
+
+                    tmp_name = join(Folder.TMP,f"TMP_{self.module}-{self.batch_date.strftime('%d%m%y')}.xlsx")
                     record.update({"input_dir": tmp_name,"function": "write_data_to_tmp_file","state": state})
+
                     ## get new data.
                     change_df = pd.DataFrame(record["data"])
                     change_df["remark"] = "Inserted"
@@ -217,7 +217,13 @@ class convert_2_files:
 
                     except FileNotFoundError:
                         ## copy files from template.
-                        state = self.copy_worksheet(template_name, tmp_name)
+                        template_name = join(Folder.TEMPLATE, "Application Data Requirements.xlsx")
+                        try:
+                            if not glob.glob(tmp_name, recursive=True):
+                                state = shutil.copy2(template_name, tmp_name)
+                            state = "succeed"
+                        except:
+                            raise
                         workbook = openpyxl.load_workbook(tmp_name)
                         sheet = workbook.worksheets[0]
                         sheet_name = "RUN_TIME_1"
@@ -254,7 +260,6 @@ class convert_2_files:
             if "errors" in record:
                 raise CustomException(errors=self.logging)
 
-
     def write_worksheet(self, sheet: any, change_data: dict) -> str:
 
         self.logging[-1].update({"function": "write_worksheet"})
@@ -268,15 +273,18 @@ class convert_2_files:
                 sheet.cell(row=1, column=idx).value = columns
             ## write data.
             while start_rows <= max_rows:
-                for remark in [change_data[start_rows][columns] for columns in change_data[start_rows].keys() if columns == "remark"]:
+                for remark in [
+                    change_data[start_rows][columns]
+                    for columns in change_data[start_rows].keys()
+                    if columns == "remark"
+                ]:
                     for idx, values in enumerate(change_data[start_rows].values(), 1):
 
                         if start_rows in self.remove_rows and remark == "Removed":
                             ## Removed
                             sheet.cell(row=start_rows, column=idx).value = values
-                            sheet.cell(row=start_rows, column=idx).font = Font(bold=True, 
-                                                                            strike=True,
-                                                                            color="00FF0000")
+                            sheet.cell(row=start_rows, column=idx).font = Font(bold=True,\
+                                strike=True, color="00FF0000")
                             show = f"{remark} Rows: ({start_rows}) in Tmp files."
 
                         elif start_rows in self.change_rows.keys() and remark in ["Inserted","Updated"]:
@@ -296,25 +304,13 @@ class convert_2_files:
         except KeyError as err:
             raise KeyError(f"Can not Write rows: {err} in Tmp files.")
         return state
-    
-    
-    def copy_worksheet(self, source_name: str, target_name: str) -> str:
-        try:
-            if not glob.glob(target_name, recursive=True):
-                shutil.copy2(source_name, target_name)
-            state = "succeed"
-        except FileNotFoundError as err:
-            raise FileNotFoundError(err)
-        return state
-
 
     def remove_row_empty(self, sheet: any) -> None:
         for row in sheet.iter_rows():
             if not all(cell.value for cell in row):
                 sheet.delete_rows(row[0].row, 1)
                 self.remove_row_empty(sheet)
-    
-    
+
     def validation_data(self, df: pd.DataFrame, change_df: pd.DataFrame) -> dict:
 
         logging.info("Verify Changed information..")
@@ -323,21 +319,24 @@ class convert_2_files:
         self.change_rows = {}
         self.remove_rows = []
         if len(df.index) > len(change_df.index):
-            self.remove_rows = [idx for idx in list(df.index) if idx not in list(change_df.index)]
+            self.remove_rows = [idx for idx in list(df.index)\
+                if idx not in list(change_df.index)]
 
         ## reset index data.
         union_index = np.union1d(df.index, change_df.index)
         ## target / tmp data.
         df = df.reindex(index=union_index, columns=df.columns).iloc[:, :-1]
         ## change data.
-        change_df = change_df.reindex(index=union_index,columns=change_df.columns).iloc[:, :-1]
+        change_df = change_df.reindex(index=union_index, columns=change_df.columns).iloc[:, :-1]
         # compare data rows by rows.
         df["count_change"] = pd.DataFrame(np.where(df.ne(change_df), True, False), index=df.index, columns=df.columns)\
             .apply(lambda x: (x == True).sum(), axis=1)
 
         def format_record(record):
-            return ("{"+ "\n".join("{!r}: {!r},".format(columns, values)\
-                for columns, values in record.items())+"}")
+            return ("{"
+                + "\n".join("{!r}: {!r},".format(columns, values)\
+                    for columns, values in record.items())
+                + "}")
 
         start_rows = 2
         for idx in union_index:
@@ -350,7 +349,7 @@ class convert_2_files:
                     if df.loc[idx, "count_change"] != 14:
 
                         ## No_changed rows.
-                        if df.loc[idx, "count_change"] < 1: # <=1
+                        if df.loc[idx, "count_change"] < 1:  # <=1
                             df.at[idx, data[0]] = data[1].iloc[idx]
                             df.loc[idx, "remark"] = "No_changed"
 
@@ -382,35 +381,34 @@ class convert_2_files:
         self.logging[-1].update({"status": "verify"})
         return _data
 
-
-    def customize_data(self, select_date: list, target_df: pd.DataFrame, tmp_df: pd.DataFrame) -> dict:
+    def customize_data(self, date: list, target_df: pd.DataFrame, tmp_df: pd.DataFrame) -> dict:
 
         logging.info("Customize Data to Target..")
         self.logging[-1].update({"function": "customize_data"})
 
         try:
             ## unique_date.
-            unique_date = target_df[target_df["CreateDate"].isin(select_date)]\
-                .reset_index(drop=True)
+            unique_date = target_df[target_df["CreateDate"].isin(date)].reset_index(drop=True)
             ## other_date.
-            other_date = (target_df[~target_df["CreateDate"].isin(select_date)]\
-                .iloc[:, :-1].to_dict("index"))
+            other_date = (target_df[~target_df["CreateDate"].isin(date)].iloc[:, :-1].to_dict("index"))
             max_rows = max(other_date, default=0)
-            
+
             ## compare data target / tmp.
             compare_data = self.validation_data(unique_date, tmp_df)
 
             ## add value to other_date.
-            other_date = other_date | {max_rows+ key: ({**values, **{"mark_rows": key}} \
-                if key in self.change_rows or key in self.remove_rows else values) \
+            other_date = other_date | {max_rows+ key: ({**values, **{"mark_rows": key}}
+                if key in self.change_rows or key in self.remove_rows else values)
                 for key, values in compare_data.items()}
 
             ## sorted date order.
             start_row = 2
-            merge_data = {start_row + idx: values for idx, values in enumerate(sorted(other_date.values(),\
-                key=lambda x: x["CreateDate"]))}
+            _data = {start_row + idx: values
+                for idx, values in enumerate(
+                sorted(other_date.values(), key=lambda x: x["CreateDate"]))}
+            
             i = 0
-            for rows, columns in merge_data.items():
+            for rows, columns in _data.items():
                 if columns.get("mark_rows"):
                     if columns["mark_rows"] in self.change_rows:
                         self.change_rows[f"{rows}"] = self.change_rows.pop(columns["mark_rows"])
@@ -421,71 +419,54 @@ class convert_2_files:
 
         except Exception as err:
             raise Exception(err)
-
-        return merge_data
-
+        return _data
 
     async def write_data_to_target_file(self) -> None:
 
         logging.info("Write Data to Target files..")
 
-        template_name = join(Folder.TEMPLATE, "Application Data Requirements.xlsx")
-        target_name = join(self.output_dir, self.output_file)
-        print(target_name)
-        
-        # state = "failed"
-        # for record in self.logging:
-        #     try:
-        #         if record["module"] == "Target_file":
-        #             record.update({"function": "write_data_to_target_file", "state": state})
+        state = "failed"
+        for record in self.logging:
+            try:
+                if record["module"] == "Target_file":
+                    record.update({"function": "write_data_to_target_file", "state": state})
+
+                    if self.write_mode == "overwrite" or self.manual:
+                        target_name = join(self.output_dir, self.output_file)
+                    else:
+                        suffix = f"{self.batch_date.strftime('%d%m%y')}"
+                        self.output_file = f"{Path(self.output_file).stem}_{suffix}.csv"
+                        target_name = join(self.output_dir, self.output_file)
+
+                    record.update({"input_dir": target_name})
+                    ## read target file.
+                    try:
+                        target_df = pd.read_csv(target_name)
+
+                    except FileNotFoundError:
+                        template_name = join(Folder.TEMPLATE, "Application Data Requirements.xlsx")
+                        target_df = pd.read_excel(template_name)
+                        target_df.to_csv(target_name, index=None, header=True)
+
+                    # target file.
+                    target_df['remark'] = "Inserted"
                     
-        #             if self.store_tmp is True:
-        #                 tmp_name = record["input_dir"]
-        #                 sheet_name = record["sheet_name"]
-        #                 df = pd.read_excel(tmp_name, sheet_name=sheet_name)
-        #                 df = df.loc[df['remark'] != "Removed"]
-        #             else:
-        #                 df = pd.DataFrame(record["data"])
-        #                 df["remark"] = "Inserted"
+                    # tmp file or column mapping.
+                    if self.store_tmp is True:
+                        tmp_name = record["input_dir"]
+                        sheet_name = record["sheet_name"]
+                        tmp_df = pd.read_excel(tmp_name, sheet_name=sheet_name)
+                        tmp_df = tmp_df.loc[tmp_df['remark'] != "Removed"]
+                    else:
+                        tmp_df = pd.DataFrame(record["data"])
+                        tmp_df["remark"] = "Inserted"
                         
-        #             print(df)
-        #             try:
-        #                 if self.write_mode == "overwrite" or self.manual:
-        #                     target_name = join(self.output_dir, f"{self.module}.csv")
-        #                 else:
-        #                     suffix = f"{self.batch_date.strftime('%d%m%y')}"
-        #                     # Folder._FILE = f"{Path(Folder._FILE).stem}_{suffix}.xlsx"
-        #                     # target_name = join(Folder.EXPORT, Folder._FILE)
+                    ## compare data tmp data with target data.
+                    date = tmp_df['CreateDate'].unique()
+                    _data = self.customize_data(date, target_df, tmp_df)
+            
+            except Exception as err:
+                record.update({"errors": err})
 
-        #                 # # check files is exist.
-        #                 # status = (
-        #                 #     "succeed"
-        #                 #     if glob.glob(target_name, recursive=True)
-        #                 #     else self.copy_worksheet(source_name, target_name)
-        #                 # )
-
-        #                 # if status == "succeed":
-        #                 #     workbook = openpyxl.load_workbook(target_name)
-        #                 #     get_sheet = workbook.get_sheet_names()
-        #                 #     sheet = workbook.get_sheet_by_name(get_sheet[0])
-        #                 #     workbook.active = sheet
-
-        #                 # ## read target file.
-        #                 # data = sheet.values
-        #                 # columns = next(data)[0:]
-        #                 # target_df = pd.DataFrame(data, columns=columns)
-        #                 # target_df["remark"] = "Inserted"
-
-        #                 # ## compare data tmp data with target data.
-        #                 # select_date = tmp_df["CreateDate"].unique()
-        #                 # new_data = self.customize_data(select_date, target_df, tmp_df)
-
-        #                 # key.update({"full_path": target_name})
-        #             except Exception as err:
-        #                     raise Exception(err)
-                
-        #     except Exception as err:
-        #         record.update({"errors": err})
-                
-        # if "errors" in record:
-        #     raise CustomException(errors=self.logging)
+        if "errors" in record:
+            raise CustomException(errors=self.logging)
