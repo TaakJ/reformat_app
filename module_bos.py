ecc0e7305bf67924d066de180e971f98f664c0ba
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-06-18 18:29:03 +0700

    for testing

--- module_bos.py
+++ module_bos.py
@@ -39,14 +39,13 @@ class module_bos(call_function):
         
         result = {"module": self.module, "task": "Completed"}
         try:
-            ''
-            # await self.check_source_files()
-            # await self.retrieve_data_from_source_files()
-            # # await self.mapping_column()
-            # await self.mock_data()
-            # if self.store_tmp is True:
-            #     await self.write_data_to_tmp_file()
-            # await self.write_data_to_target_file()
+            await self.check_source_files()
+            await self.retrieve_data_from_source_files()
+            # await self.mapping_column()
+            await self.mock_data()
+            if self.store_tmp is True:
+                await self.write_data_to_tmp_file()
+            await self.write_data_to_target_file()
                 
         except CustomException as error: 
             
