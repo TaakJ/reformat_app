09276baab9c8bca46c49ac3937d959044b791a78
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-06-22 23:34:37 +0700

    for test

--- exception.py
+++ exception.py
@@ -5,11 +5,8 @@ class CustomException(Exception):
         for key, value in self.__dict__.items():
             setattr(self, key, value)
             
-        print(self.__dict__)
+        self.err_msg = self.generate_error()
         
-        # self.msg_err = self.generate_error()
-        # print(self.msg_err)
-    
     def __iter__(self):
         return self
 
@@ -17,16 +14,14 @@ class CustomException(Exception):
         return next(self.msg_err)
 
     def generate_error(self):
-        a = self.err
-        print(a)
         try:
             for i in range(len(self.err)):
-                error_message = f'''Module::{self.errors[i]["module"]} 
-                                , Path::{self.errors[i]["input_dir"]}
-                                , Function::{self.errors[i]["function"]}
-                                , Status::{self.errors[i]["state"]}
-                                , Error::{self.errors[i].get("errors", "No Error")}
-                                '''
-                yield error_message
+                err_msg = f'''Module::{self.err[i]["module"]} 
+                            , Path::{self.err[i]["input_dir"]}
+                            , Function::{self.err[i]["function"]}
+                            , Status::{self.err[i]["state"]}
+                            , Error::{self.err[i].get("errors", "No Error")}
+                            '''
+                yield err_msg
         except:
             pass
