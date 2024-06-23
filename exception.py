60f199f2f1fd996e57db15edbdaf9efb6014dd04
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-06-23 15:26:04 +0700

    for exception

--- exception.py
+++ exception.py
@@ -12,9 +12,10 @@ class CustomException(Exception):
         
         for key, value in self.__dict__.items():
             setattr(self, key, value)
-            
+        
         self.x = args[0]
         self.err_msg = self.generate_error()
+
         
     def __iter__(self):
         return self
@@ -22,39 +23,53 @@ class CustomException(Exception):
     def __next__(self):
         return next(self.err_msg)
     
-    def setup_error(self) -> None:
-        config_yaml  = None
-        date = datetime.today().strftime("%d%m%y")
-        file = "_error"
-        filename = Folder.LOG + join(date, file)
+    # def setup_error(self) -> None:
+    #     config_yaml  = None
+    #     date = datetime.today().strftime("%d%m%y")
+    #     file = "_error"
+    #     filename = Folder.LOG + join(date, file)
         
-        if not os.path.exists(os.path.dirname(filename)):
-            try:
-                os.makedirs(os.path.dirname(filename))
-            except OSError:
-                pass
+    #     if not os.path.exists(os.path.dirname(filename)):
+    #         try:
+    #             os.makedirs(os.path.dirname(filename))
+    #         except OSError:
+    #             pass
             
-        if os.path.exists(Folder._LOGGER_CONFIG_DIR):
-            with open(Folder._LOGGER_CONFIG_DIR, 'rb') as logger:
-                config_yaml  = yaml.safe_load(logger.read())
+    #     if os.path.exists(Folder._LOGGER_CONFIG_DIR):
+    #         with open(Folder._LOGGER_CONFIG_DIR, 'rb') as logger:
+    #             config_yaml  = yaml.safe_load(logger.read())
                 
-                for i in (config_yaml["handlers"].keys()):
-                    if "filename" in config_yaml['handlers'][i]:
-                        # config_yaml["handlers"][i]["level"] = "ERROR"
-                        # config_yaml["handlers"][i]["mode"] = "a"
-                        config_yaml["handlers"][i]["filename"] = filename
+    #             for i in (config_yaml["handlers"].keys()):
+    #                 if "filename" in config_yaml['handlers'][i]:
+    #                     # config_yaml["handlers"][i]["level"] = "ERROR"
+    #                     # config_yaml["handlers"][i]["mode"] = "a"
+    #                     config_yaml["handlers"][i]["filename"] = filename
                         
-                config_yaml["root"]["level"] = "CRITICAL"
+    #             config_yaml["root"]["level"] = "CRITICAL"
                 
-                logging.config.dictConfig(config_yaml)
-        else:
-            raise Exception(f"Yaml file file_path: '{Folder._LOGGER_CONFIG_DIR}' doesn't exist.")
+    #             logging.config.dictConfig(config_yaml)
+    #     else:
+    #         raise Exception(f"Yaml file file_path: '{Folder._LOGGER_CONFIG_DIR}' doesn't exist.")
             
+    # def setup_error(self, log_name = '' , log_file_error  = "_error"):
 
-    def generate_error(self) -> any:
-        self.setup_error()
-        yield self.x
+    #     log           = logging.getLogger(log_name)
+    #     # log_formatter = logging.Formatter(log_format)
         
+    #     file_handler_error = logging.FileHandler(log_file_error, mode='w')
+    #     # file_handler_error.setFormatter(log_formatter)
+    #     file_handler_error.setLevel(logging.ERROR)
+    #     log.addHandler(file_handler_error)
+        
+    #     log.setLevel(logging.INFO)
+        
+    #     return log
+
+    
+    def generate_error(self) -> any:
+        # _logging = self.setup_error()
+        
+        yield self.x
         
         # try:
         #     for i in range(len(self.err)):
