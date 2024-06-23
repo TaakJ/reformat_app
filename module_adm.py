5c7c57cfd46b9bbe42cbf43c9eb280f119a62616
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-06-24 01:17:40 +0700

    for test

--- module_adm.py
+++ module_adm.py
@@ -65,7 +65,7 @@ class module_adm(call_function):
             mock_data = [['ApplicationCode',	'AccountOwner', 'AccountName',	'AccountType',	'EntitlementName',	'SecondEntitlementName','ThirdEntitlementName', 'AccountStatus',	'IsPrivileged',	'AccountDescription',
                         'CreateDate','LastLogin','LastUpdatedDate',	'AdditionalAttribute'],
                         ["TAAK","2","3","4","5","6","7","8","9","10",self.fmt_batch_date, self.date, self.fmt_batch_date,"14"],
-                        # ["TOKE","16","17","18","19","20","21","22","23","24",self.fmt_batch_date, self.date, self.fmt_batch_date,"28"],
+                        ["TOKE","16","17","18","19","20","21","22","23","24",self.fmt_batch_date, self.date, self.fmt_batch_date,"28"],
                         ]
             df = pd.DataFrame(mock_data)
             df.columns = df.iloc[0].values
