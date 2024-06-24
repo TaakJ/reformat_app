118a7af8cc9a3ded9ce64022cd5430ee37b794b3
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-06-24 14:31:44 +0700

    for test

--- exception.py
+++ exception.py
@@ -23,7 +23,7 @@ class CustomException(Exception):
         try:
             for i in range(len(self.err)):
                 if self.err[i].get("err") is not None:
-                    err_msg = f'Module::"{self.err[i]["module"]}",\nPath::"{self.err[i]["input_dir"]}",\nFunction::"{self.err[i]["function"]}",\nStatus::"{self.err[i]["state"]}",\nErrors::"{self.err[i]["err"]}"\n'
+                    err_msg = f'Module::"{self.err[i]["module"]}", Path::"{self.err[i]["input_dir"]}", Function::"{self.err[i]["function"]}", Status::"{self.err[i]["state"]}", Errors::"{self.err[i]["err"]}"\n'
                     yield err_msg
         except:
             raise
