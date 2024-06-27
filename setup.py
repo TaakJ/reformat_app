f15763d289c9c62987a987b6a9181297cab2641b
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-06-27 15:43:58 +0700

    for test

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.1",
     packages=find_packages(),
     description="build package reformat uob app",
-    # entry_point={"console_script":["reformat_app:uob-app:main"]},
+    entry_points={"console_script":["reformat_app:reformat_app:main"]},
     )
