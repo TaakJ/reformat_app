d5d8684ba57ec61528e22f9dcc9293a220e81c6b
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-06-28 01:36:33 +0700

    for test

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="reformat_app", 
+    name="reformat_app",
     version="0.1",
     packages=find_packages(),
     description="build package reformat uob app",
     classifiers=[
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    install_requires=[r.strip() for r in open("requirements.txt").readlines()],
+    python_requires=">=3.9",
     entry_points={
-        "console_scripts":[
+        "console_scripts": [
             "reformat_app = reformat_app:main",
         ],
     },
-    )
+)
