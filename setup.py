261e8b76f95f08ef50e4b355eb721b066db2cdd3
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2024-06-27 18:14:39 +0700

    for test

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ setup(
     version="0.1",
     packages=find_packages(),
     description="build package reformat uob app",
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
     entry_points={
         "console_scripts":[
             "reformat_app = reformat_app:main",
