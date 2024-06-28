06d414b43b8048b0b90a7a5944c4b1f72b14fe59
thanakit <tak_control14@hotmail.com>
2024-06-28 18:35:43 +0700

    for deploy

--- setup.py
+++ setup.py
@@ -12,9 +12,4 @@ setup(
     ],
     install_requires=[r.strip() for r in open("requirements.txt").readlines()],
     python_requires=">=3.9",
-    # entry_points={
-    #     "console_scripts": [
-    #         "reformat_app = reformat_app:main",
-    #     ],
-    # }
 )
--- src/app.py
+++ src/app.py
@@ -28,8 +28,8 @@ from PyQt6.QtCore import (
     pyqtSignal,
 )
 from qt_material import apply_stylesheet
-from .setup import Folder, PARAMS, CONFIG
-from .main import start_app
+from setup import Folder, PARAMS, CONFIG
+from main import start_app
 
 class Jobber(QObject):
     set_total_progress = pyqtSignal(int)
