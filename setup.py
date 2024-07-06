c6dacfb051ba6379700b7731c00149707cb8738c
thanakit <tak_control14@hotmail.com>
2024-07-06 14:36:52 +0700

    for deploy pypi

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from setuptools import setup, find_packages
 
-with open('README.md', encoding='utf-8') as f:
-    long_description = f.read()
-install_requires = [r.strip() for r in open("requirements.txt").readlines()]
-
 setup(
     name="reformat_app",
     version="0.0.1",
-    packages=find_packages(),
     description="build package reformat uob app",
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    author='Thanakit Jitcharoenjai',
+    author_email='tak_control14@hotmail.com',
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=install_requires,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    keywords='',
+    packages=find_packages(),
+    python_requires=">=3.9",
+    install_requires=[r.strip() for r in open("requirements.txt").readlines()],
     # entry_points={
     #     "console_scripts":[
     #         "reformat-app = reformat_app:main",
     #     ]
     # },
-    python_requires=">=3.9",
 )
