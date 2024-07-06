8a2e4539e5a2618a171d6184e1908700bb7c445e
thanakit <tak_control14@hotmail.com>
2024-07-06 13:07:49 +0700

    for depoly

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+    
+install_requires = [r.strip() for r in open("requirements.txt").readlines()]
+
 setup(
     name="reformat_app",
     version="0.0.1",
@@ -10,7 +15,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[r.strip() for r in open("requirements.txt").readlines()],
+    install_requires=install_requires,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     entry_points={
         "console_scripts":[
             "reformat-app = reformat_app:main",
