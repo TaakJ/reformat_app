484013c2f367d8ff2c928927335d7c63c6939925
thanakit <tak_control14@hotmail.com>
2024-07-13 23:45:33 +0700

    for test

--- uob_reformat/exception.py
+++ uob_reformat/exception.py
@@ -1,4 +1,5 @@
 import os
+import types
 from os.path import join
 import logging
 from datetime import datetime
@@ -22,7 +23,18 @@ class CustomException(Exception):
 
     def __next__(self):
         return next(self.err_msg)
+    
+    def log_newline(self, how_many_lines=1):
+        # Switch handler, output a blank line
+        self.removeHandler(self.file_handler)
+        self.addHandler(self.blank_handler)
+        for i in range(how_many_lines):
+            self.info('')
 
+        # Switch back
+        self.removeHandler(self.blank_handler)
+        self.addHandler(self.file_handler)
+    
     def setup_errorlog(self,
         log_format="%(asctime)s.%(msecs)03d | %(module)10s | %(levelname)8s | %(funcName)20s | %(message)s",
         log_name="", 
@@ -36,16 +48,28 @@ class CustomException(Exception):
             except OSError:
                 pass
         
-        errorlog = logging.getLogger(log_name)
+        ## Create file error handler
         file_handler = logging.FileHandler(filename, mode="a")
+        file_handler.setLevel(logging.ERROR)
         formatter = logging.Formatter(fmt=log_format,datefmt="%Y/%m/%d %H:%M:%S")
         file_handler.setFormatter(formatter)
-        file_handler.setLevel(logging.ERROR)
         
-        errorlog.addHandler(file_handler)
-        errorlog.setLevel(logging.INFO)
+        ## Create a "blank line" handler
+        blank_handler = logging.FileHandler(filename, mode="a")
+        blank_handler.setLevel(logging.DEBUG)
+        blank_handler.setFormatter(logging.Formatter(fmt=''))
         
-        return errorlog
+        ## Create a logger, with the previously-defined handler
+        logger = logging.getLogger(log_name)
+        logger.setLevel(logging.INFO)
+        logger.addHandler(file_handler)
+            
+        ## Save some data and add a method to logger object
+        logger.file_handler = file_handler
+        logger.blank_handler = blank_handler
+        logger.newline = types.MethodType(self.log_newline, logger)
+        
+        return logger
 
     def generate_error(self) -> any:
         for i in range(len(self.err)):
