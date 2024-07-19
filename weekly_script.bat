8c81ad9e31e6ba2140076c871560bc0c3dacc4e8
thanakit <tak_control14@hotmail.com>
2024-07-19 11:34:37 +0700

    for test

--- weekly_script.bat
+++ weekly_script.bat
@@ -11,13 +11,13 @@ goto exit
 :function
 
 :: Activate the virtual environment
-REM call %cd%\.venv\Scripts\activate.bat
+call %cd%\.venv\Scripts\activate.bat
 
 :: Main script
 call uob_reformat %params%
 
 :: Deactivate the virtual environment
-REM call  %cd%\.venv\Scripts\deactivate.bat
+call  %cd%\.venv\Scripts\deactivate.bat
 
 cd /d %default_dir%
 
