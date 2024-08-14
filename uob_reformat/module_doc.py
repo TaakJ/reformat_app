d24ebf2f572aeee5145cf2b87db535562539e385
thanakit <tak_control14@hotmail.com>
2024-08-14 18:29:21 +0700

    for test

--- uob_reformat/module_doc.py
+++ uob_reformat/module_doc.py
@@ -26,9 +26,9 @@ class ModuleDOC(CallFunction):
 
             await self.check_source_file()
             await self.separate_data_file()
-            # if self.store_tmp is True:
-            #     await self.genarate_tmp_file()
-            # await self.genarate_target_file()
+            if self.store_tmp is True:
+                await self.genarate_tmp_file()
+            await self.genarate_target_file()
 
         except CustomException as err:
             logging.error('See Error Details: log_error.log')
