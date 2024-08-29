fb00ab08e6a458f22d04fff72425cab2c1636904
Thanakit Jitchatoenjai <97144359+TaakJ@users.noreply.github.com>
2024-08-29 15:53:23 +0700

    for test

--- uob_reformat/module_adm.py
+++ uob_reformat/module_adm.py
@@ -4,6 +4,7 @@ import logging
 from .function import CallFunction
 from .exception import CustomException
 
+
 class ModuleADM(CallFunction):
 
     def __init__(self, params: any) -> None:
@@ -17,7 +18,7 @@ class ModuleADM(CallFunction):
 
         logging.info(f"Module:'{self.module}'; Manual: '{self.manual}'; Run date: '{self.batch_date}'; Store tmp: '{self.store_tmp}'; Write mode: '{self.write_mode}';")
 
-        result = {'module': self.module, 'task': 'Completed'}
+        result = {"module": self.module, "task": "Completed"}
         try:
             self.colloct_setup()
 
@@ -26,12 +27,12 @@ class ModuleADM(CallFunction):
 
             await self.check_source_file()
             await self.separate_data_file()
-            # if self.store_tmp is True:
-            #     await self.genarate_tmp_file()
+            if self.store_tmp is True:
+                await self.genarate_tmp_file()
             # await self.genarate_target_file()
 
         except CustomException as err:
-            logging.error('See Error Details: log_error.log')
+            logging.error("See Error Details: log_error.log")
 
             logger = err.setup_errorlog(log_name=__name__)
             while True:
@@ -40,7 +41,7 @@ class ModuleADM(CallFunction):
                 except StopIteration:
                     break
 
-            result.update({'task': 'Uncompleted'})
+            result.update({"task": "Uncompleted"})
 
         logging.info(f"Stop Run Module '{self.module}'\r\n")
 
@@ -49,75 +50,100 @@ class ModuleADM(CallFunction):
     def collect_user(self, i: int, format_file: any) -> dict:
 
         status = "failed"
-        self.logging[i].update({'function': 'collect_user', 'status': status})
-        
+        self.logging[i].update({"function": "collect_user", "status": status})
+
         try:
             data = []
             for line in format_file:
-                data += [re.sub(r'(?<!\.)\|\|', '||', line.strip()).split("||")]
-            
+                data += [re.sub(r'(?<!\.)\|\|', '||', line.strip()).split('||')]
+
             ## set dataframe
             df = pd.DataFrame(data)
             df = df.apply(lambda row: row.str.strip()).reset_index(drop=True)
-            
+
             ## mapping data to column
+            # 0: User-ID
+            # 1: User Full Name
+            # 2: Department code
+            # 3: Employee ID
+            # 4: Group
+            # 5: Zone
+            # 6: Role
             df = df.groupby(0)
             df = df.agg(lambda row: '+'.join(row.unique())).reset_index()
-            
+
             set_value = dict.fromkeys(self.logging[i]['columns'], 'NA')
             set_value.update({
-                'ApplicationCode': 'ADM', 
+                'ApplicationCode': 'ADM',
                 'AccountOwner': df[0],
                 'AccountName': df[0],
                 'AccountType': 'USR',
-                'EntitlementName': df[[4, 5, 6]].apply(lambda row: ';'.join(row), axis=1),
+                'EntitlementName': df[[4, 6, 5]].apply(lambda row: ";".join(row), axis=1),
                 'AccountStatus': 'A',
                 'IsPrivileged': 'N',
                 'AccountDescription': df[1],
                 'AdditionalAttribute': df[2],
-                'Country': 'TH'
+                'Country': 'TH',
             })
-            df = df.assign(**set_value).fillna('NA') 
-            df = df.drop(df.iloc[:,:7].columns, axis=1)
-            
+            df = df.assign(**set_value).fillna('NA')
+            df = df.drop(df.iloc[:, :7].columns, axis=1)
+
         except Exception as err:
             raise Exception(err)
-        
+
         status = 'succeed'
         self.logging[i].update({'data': df.to_dict('list'), 'status': status})
-        logging.info(f"Collect user data, status: {status}")
-        
+        logging.info(f'Collect user data, status: {status}')
+
     def collect_param(self, i: int, format_file: any) -> dict:
-        
+
         status = 'failed'
         self.logging[i].update({'function': 'collect_param', 'status': status})
-        
+
         try:
             data = []
             for line in format_file:
-                data += [re.sub(r'(?<!\.)\|\|', '||', line.strip()).split("||")]
-            
+                data += [re.sub(r"(?<!\.)\|\|", "||", line.strip()).split("||")]
+
             ## set dataframe
             df = pd.DataFrame(data)
-            
+            df = df.apply(lambda row: row.str.strip()).reset_index(drop=True)
+
             ## mapping data to column
-            # df = df.groupby(4)
-            # df = df.agg(lambda row: ';'.join(row.unique())).reset_index()
-            # print(df[[4,5,6]])
-            
-            # set_value = dict.fromkeys(self.logging[i]['columns'], 'NA')
-            # set_value.update({
-            #     'Parameter Name': df[4].unique(), 
-            #     'Code value': df[5].unique(),
-            #     'Decode value': df[6].unique(),
-            # })
-            # df = df.from_dict(set_value, orient='column')
-            # print(df)
-            # df = df.drop(df.iloc[:,:7].columns, axis=1)
+            # 0: User-ID
+            # 1: User Full Name
+            # 2: Department code
+            # 3: Employee ID
+            # 4: Group
+            # 5: Zone
+            # 6: Role
+            set_value = [
+                {
+                    'Parameter Name': 'GroupDetail', 
+                    'Code value': df[4].unique(), 
+                    'Decode value': df[4].unique()
+                },
+                {
+                    'Parameter Name': 'RoleDetail', 
+                    'Code value': df[6].unique(), 
+                    'Decode value': df[6].unique()
+                },
+                {
+                    'Parameter Name': 'Zone', 
+                    'Code value': df[5].unique(), 
+                    'Decode value': df[5].unique()
+                },
+                {
+                    'Parameter Name': 'Department', 
+                    'Code value': df[2].unique(), 
+                    'Decode value': df[2].unique()
+                }]
+            df = pd.DataFrame(set_value)
+            df = df.explode(['Code value', 'Decode value']).reset_index(drop=True)
             
         except Exception as err:
             raise Exception(err)
-        
+
         status = 'succeed'
-        self.logging[i].update({'data': df.to_dict('list'), 'status': status})
-        logging.info(f"Collect param data, status: {status}")
+        self.logging[i].update({"data": df.to_dict('list'), 'status': status})
+        logging.info(f'Collect param data, status: {status}')
